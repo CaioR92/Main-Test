f63dd2cedf5169de4d33629ac41e77a976ddccd2
Caio Silva <caioricardo.92@gmail.com>
2024-08-29 18:26:50 -0300

    falta revisar MENU e INPUT

--- Desafio_de_codigo_1.py
+++ Desafio_de_codigo_1.py
@@ -6,32 +6,77 @@
 # Histórico de saques no extrato
 # Exibir saldo atual da conta no fim do extrato
 
-menu = print("""
-Olá, f'{nome}. Bem-vindo à sua conta. Por getileza, escolha uma opção
-
-[1] Depositar
-[2] Sacar
-[3] Ver extrato
-[4] Sair
-
-"""
-)
-
 saldo = 0
 limite = 500
 extrato = ""
 numero_saque = 0
 LIMITE_SAQUE = 3
+historico_saque = []
+historico_deposito = []
 
 def deposito():
-    print("Qual valor deseja depositar?")
-    depositar = int(input()
-        if int(input) ≤ 0:
-            print("Não é possível depositar este valor.")
-        else
-            print(f"Você acaba de depositar {input} reais.")
-    novo_saldo = depositar + saldo
+    global saldo
+    global historico_deposito
     
+    valor = int(input("Qual valor deseja depositar? "))
+    if valor <= 0:
+        print("Não é possível depositar este valor. Tente novamente.")
+    else:
+        print(f"Você acaba de depositar R$  {valor} reais.")
+        saldo += valor
+        historico_deposito.append(valor)
 
-while (True):
-    opcao = input(menu)
+def saque():
+    global saldo
+    global numero_saque
+    
+    valor = int(input("Qual valor deseja sacar?"))
+    if valor <= 0:
+        print("Não é possível sacar este valor. Tene novamente.")
+    elif valor > saldo:
+        print("Não é possível sacar este valor. Saldo indisponível.")
+    elif (valor > limite) or (numero_saque >= LIMITE_SAQUE):
+        print("Limite de saque atingido.")
+    else:
+        saldo -= valor
+        numero_saque += 1
+        historico_saque.append(valor)
+        print(f"Saldo efetuado com sucesso. Você acaba de sacar R$ {valor} reais.")
+
+# VERIFICAR MENU E INPUT
+
+nome = input("Qual seu nome?")
+
+while(True):
+    menu = print(f"""
+    Olá, {nome}. Bem-vindo à sua conta. Por getileza, escolha uma opção:\n
+
+    [1] Depositar\n
+    [2] Sacar\n
+    [3] Ver extrato\n
+    [4] Sair\n
+
+    """)
+    
+    opcao = int(input(menu))
+    if opcao == 1:
+        deposito()
+    elif opcao == 2:
+        saque()
+    elif opcao == 3:
+        extrato = print(f"""----- Extrato de sua conta bancária -----
+          
+          Histórico de Depósito = {historico_deposito}
+          
+          Histórico de Saque = {historico_saque}
+          
+          Saldo atual = {saldo}
+          
+          Obrigado por usar os nossos serviço. Volte sempre!
+          
+          """)
+    
+    elif opcao == 4:
+        print("Obrigado por usar os nossos serviço. Volte sempre!")
+    else:
+        print("Opção inválida. Por favor, escolha entre as seguintes opções: [1] Depositar, [2] Sacar, [3] Ver extrato, [4] Sair\n")
