41a78b258078959070cb7fd0bc2974890bbf98b8
Caio Silva <caioricardo.92@gmail.com>
2024-09-02 08:24:59 -0300

    Final

--- Desafio_de_codigo_1.py
+++ Desafio_de_codigo_1.py
@@ -18,43 +18,40 @@ historico_deposito = []
 def deposito():
     global saldo
     global historico_deposito
-    # global extrato
     
-    valor = float(input("\nQual valor deseja depositar? R$ "))
-    if valor <= 0:
-        print("\nNão é possível depositar este valor. Tente novamente.\n")
-    else:
-        saldo += valor
-        historico_deposito.append(valor)
-        # extrato += f"Depósito: R$ {valor:.2f}\n"
-        print(f"\nVocê acaba de depositar: R$ {valor:.2f} reais.")
-    # Adicionar linha para inputs que não sejam números
-
+    try:
+        valor = float(input("\nQual valor deseja depositar? R$ "))
+        if valor <= 0:
+            print("\nNão é possível depositar este valor. Tente novamente.\n")
+        else:
+            saldo += valor
+            historico_deposito.append(valor)
+            print(f"\nVocê acaba de depositar: R$ {valor:.2f} reais.")
+    except ValueError:
+        print("\nEntrada inválida. Por favor, insira um valor numérico.\n")
 
 def saque():
     global saldo
     global numero_saque
-    # global extrato
     
-    valor = float(input("Qual valor deseja sacar? R$ "))
-    if valor <= 0:
-        print("\nNão é possível sacar este valor. Tene novamente.\n")
-    elif valor > saldo:
-        print("\nNão é possível sacar este valor. Saldo indisponível.\n")
-    elif (valor > limite) or (numero_saque >= LIMITE_SAQUE):
-        print("\nLimite de saque atingido.\n")
-    else:
-        saldo -= valor
-        numero_saque += 1
-        historico_saque.append(valor)
-        # extrato += f"Saque: R$ {valor:.2f}\n"
-        print(f"\nSaque efetuado com sucesso. Você acaba de sacar: R$ {valor:.2f} reais.")
-    # Adicionar linha para inputs que não sejam números
+    try:
+        valor = float(input("Qual valor deseja sacar? R$ "))
+        if valor <= 0:
+            print("\nNão é possível sacar este valor. Tente novamente.\n")
+        elif valor > saldo:
+            print("\nNão é possível sacar este valor. Saldo indisponível.\n")
+        elif (valor > limite) or (numero_saque >= LIMITE_SAQUE):
+            print("\nLimite de saque atingido. Não é possível efetuar esta ação.\n")
+        else:
+            saldo -= valor
+            numero_saque += 1
+            historico_saque.append(valor)
+            print(f"\nSaque efetuado com sucesso. Você acaba de sacar: R$ {valor:.2f} reais.")
+    except ValueError:
+        print("\nEntrada inválida. Por favor, insira um valor numérico.\n")
 
-
-# escrever def exibir_extrato
 def exibir_extrato():
-    print("\n----- Extrato de sua conta bancária -----")
+    print("\n----- Extrato de sua conta bancária -----\n")
     if not historico_deposito:
         print("\nNão houve depósitos nesta conta.")
     else:
@@ -62,16 +59,13 @@ def exibir_extrato():
         for deposito in historico_deposito:
             print(f"Depósito: R$ {deposito:.2f}\n")
     if not historico_saque:
-        print("Não houve saques nesta conta.")
+        print("\nNão houve saques nesta conta.")
     else:
         print("\nHistórico de Saque:\n")
         for saque in historico_saque:
             print(f"Saque: R$ {saque:.2f}\n")
     print(f"\nSaldo atual: = R$ {saldo:.2f}\n\nObrigado por usar os nossos serviço. Volte sempre!")
     
-
-# VERIFICAR MENU E INPUT
-
 nome = input("Qual seu nome? ")
 
 while(True):
@@ -93,8 +87,7 @@ while(True):
     elif opcao == 3:
         exibir_extrato()
     elif opcao == 4:
-        print("\nObrigado por usar os nossos serviço. Volte sempre!")
+        print("\nObrigado por usar os nossos serviços. Volte sempre!")
         break
     else:
         print("\nOpção inválida. Por favor, escolha entre as seguintes opções: [1] Depositar, [2] Sacar, [3] Ver extrato, [4] Sair\n")
-    # Adicionar linha para inputs que não sejam números
