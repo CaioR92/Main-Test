adb7526e2a49ebe7cee8a77a848d70817f1edebb
Caio Silva <caioricardo.92@gmail.com>
2024-09-19 18:00:39 -0300

    updates 19/09 18h

--- Desafio_de_codigo_OOP.py
+++ Desafio_de_codigo_OOP.py
@@ -1,12 +1,19 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+
+mascara_ptbr = "%d/%m/%Y %H:%M"
+data_hora_atual = datetime.now().strftime(mascara_ptbr)
 
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
     
-    def realizar_transacao(self, conta, transacao): #polimorfismo
-        
+    def realizar_transacao(self, conta, transacao): #polimorfismo permite que se use um método ou interface comum em várias classes de maneiras distintas
+        transacao.registrar(conta) #Passa qualquer tipo de transação genérica
+        #(ou seja, qualquer tipo de transação que implemente a interface Transacao)
+        #e o comportamento será diferente dependendo de qual subclasse de Transacao
+        #está sendo utilizada (pode ser Deposito ou Saque).
         
     def adicionar_conta(self, conta): # liente pode ter várias contas
         self.contas.append(conta)
@@ -60,7 +67,7 @@ class Conta:
             return False
         elif valor > 0:
             self.__saldo -= valor
-            print(f"Saque no valor de R$ {self.valor} efetuado com sucesso.")
+            print(f"Saque no valor de R$ {valor:.2f} efetuado com sucesso.") #Não pode ser self.valor pois o valor é um argumento, ñ um atributo
             return True
         else:
             print("Impossível realizar saque. Valor incorreto.")
@@ -70,22 +77,35 @@ class Conta:
     def depositar(self, valor: float) -> bool:
         if valor > 0:
             self.__saldo += valor
-            print(f"Deposíto no valor de R$ {self.valor} efetuado com sucesso.")
+            print(f"Deposíto no valor de R$ {valor:.2f} efetuado com sucesso.")
         else:
             print("Impossível realizar depósito. Valor incorreto.")
             return False
         return True
         
-class ContaCorrente(Conta): #herança 
-    def __init__(self, numero, saldo, cliente, limite=500, limite_saques=3):
-        super().__init__(numero, cliente, saldo)
+class ContaCorrente(Conta): #herança; com haverá diferentes tipos de conta, os controles devem ser feito nas classes descendentes
+    def __init__(self, cliente, numero, saldo, limite=500, limite_saques=3): #limites devem ser inicializados no construtor
+        super().__init__(cliente, numero, saldo)
         self.limite = limite
         self.limite_saques = limite_saques
+        self.numero_saque = 0
+        self.data_hora_atual = None
+    
+#É necessário chamar os métodos da classe base para realizar outros tipos de verificações
+#Adicionar contador de saques que é verificado a cada operação
+    def sacar(self, valor):
+        if self.numero_saque >= self.limite_saques:
+            print("\nLimite de saques diários atingido.")
+        else:
+            if super().sacar(valor): #super(). para chamar o método da classe ascendente
+                self.numero_saque += 1
+                self.data_hora_atual = datetime.now().strftime(mascara_ptbr)
+                print(f"Saque no valor de R$ {valor:.2f} efetuado com sucesso.")
 
 class Transacao(ABC): #abstrata; contém atributos e métodos comuns com outras classes derivadas
     
     @abstractmethod #não possui implementação na classe base, apenas nas derivadas (obrigadas)
-    def registrar(self, conta: Conta):
+    def registrar(self, conta: Conta): #vai ser chamada através de polimorfismo
         pass
 
 class Deposito(Transacao):
@@ -93,7 +113,8 @@ class Deposito(Transacao):
         self.valor = valor
         
     def registrar(self, conta: Conta): 
-        conta.depositar(self.valor)    
+        conta.depositar(self.valor)
+        print(f"Depositando R$ {self.valor:.2f} na conta.")
     # atributos (self, valor)
     # ganha o método registrar da classe transacao
     
@@ -103,7 +124,7 @@ class Saque(Transacao):
         
     def registrar(self, conta: Conta):
         conta.sacar(self.valor)
-    
+        print(f"Sacando R$ {self.valor:.2f} na conta.")
     #implementa o método registrar
     # atributos (self. valor)
     # ganha o método registrar da classe transacao
@@ -112,8 +133,11 @@ class Historico: #init + adicionar transaçoes em self.transacoes + atributo tra
     def __init__(self):
         self.__transacoes = []  #Lista vazia; armazena instância criadas da classe
         
-    def transacoes(trasacao: Transacao):
+    def transacoes(self, trasacao: Transacao):
         return self.transacoes
         
     def adicionar_transacao(self): #dict
-        
+        print(f"""{'Tipo de Transação': transacao,
+                    'Valor da Transação': valor,
+                    'Data da Transação': data_hora}""")
+
